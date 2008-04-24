6999c9906d7065054c6e16226c35a032e9f490ce
Yuxing Huang <gzfelix@gmail.com>
2008-04-24 15:38:30 -0300

    * change drag and drop implementation to IK programming topics example

--- YuplooPhotoViewController.m
+++ YuplooPhotoViewController.m
@@ -15,15 +15,13 @@
 
 - (void)awakeFromNib
 {
-	// register for drag and drop support
-	[browserView registerForDraggedTypes:[NSArray arrayWithObjects:NSFilenamesPboardType, nil]];
-	
 	// allocate some space for the data source
 	browserImages = [[NSMutableArray alloc] initWithCapacity:10];
 	importedImages = [[NSMutableArray alloc] initWithCapacity:10];
 	
 	// fancy view
 	[browserView setAnimates:YES];
+	[browserView setAllowsReordering:YES];
 	
 	//Browser UI setup (can also be set in IB)
 	[browserView setDelegate:self];
@@ -98,18 +96,9 @@
 
 #pragma mark - 
 #pragma mark Browser Drag and Drop Methods
-- (unsigned int)draggingEntered:(id <NSDraggingInfo>)sender
+- (NSDragOperation)draggingEntered:(id <NSDraggingInfo>)sender
 {
-	
-	if([sender draggingSource] != self){
-		NSPasteboard *pb = [sender draggingPasteboard];
-		NSString * type = [pb availableTypeFromArray:[NSArray arrayWithObject:NSFilenamesPboardType]];
-		
-		if(type != nil){
-			return NSDragOperationEvery;
-		}
-	}
-	return NSDragOperationNone;
+    return NSDragOperationCopy;
 }
 
 - (unsigned int)draggingUpdated:(id <NSDraggingInfo>)sender
@@ -122,22 +111,29 @@
 	return YES;
 }
 
-- (BOOL)performDragOperation:(id <NSDraggingInfo>)sender
+- (BOOL) performDragOperation:(id <NSDraggingInfo>)sender
 {
-	//Get the files from the drop
-	NSArray * files = [[sender draggingPasteboard] propertyListForType:NSFilenamesPboardType];
+    NSData *data = nil;
+    NSString *errorDescription;
+    NSPasteboard *pasteboard = [sender draggingPasteboard];
 	
-	[self addImagesWithPaths: files];
+    if ([[pasteboard types] containsObject:NSFilenamesPboardType])
+        data = [pasteboard dataForType:NSFilenamesPboardType];
+    if(data){
+        NSArray *filenames = [NSPropertyListSerialization
+							  propertyListFromData:data
+							  mutabilityOption:kCFPropertyListImmutable
+							  format:nil
+							  errorDescription:&errorDescription];
+        int i;
+        int n = [filenames count];
+        for(i=0; i<n; i++){
+            [self addImagesWithPath:[filenames objectAtIndex:i] recursive:NO];
+        }
+        [self updateDataSource];
+    }
 	
-	if ([importedImages count] > 0)
-		return YES;
-	else
-		return NO;
-}
-
-- (void)concludeDragOperation:(id < NSDraggingInfo >)sender
-{
-	[self updateDataSource];
+    return YES;
 }
 
 
